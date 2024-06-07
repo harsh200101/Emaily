7c2b3af48260bb6f43783fd33fae6bad923902c8
harsh200101 <harshgajbhiye34@gmail.com>
2024-06-07 11:57:35 +0530

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+
+//dev.js dont commit this 
+module.exports={
+    googleClientID: '358005051414-blckl63p5ms7m4k90nh0rqqo8476arnl.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://harshgajbhiye34:Fa86BOdlVgDElM5Z@cluster0.mkwzzty.mongodb.net/emailydb?retryWrites=true&w=majority&appName=Cluster0',
+    cookieKey: 'redacted'
+};
